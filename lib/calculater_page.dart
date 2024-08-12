e14d4440f781802d5d45a46d4f33e9f38f3a6488
George <goerge.anwer10@gmail.com>
2024-08-12 22:32:46 +0300

    update the design

---
+++ lib/calculater_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatelessWidget {
+  const CalculatorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[900],
+      appBar: AppBar(
+        title: const Text(
+            'BMI CALCULATOR',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[900],
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 250,
+                width: 180,
+                decoration:BoxDecoration(
+                  color: Colors.indigo[600],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+        ),
+              Container(
+                height: 250,
+                width: 180,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[600],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+
+
+            ],
+          )
+        ],
+      ),
+      );
+
+
+
+  }
+}
