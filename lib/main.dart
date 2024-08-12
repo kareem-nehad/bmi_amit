41fe4469105e659cf4abbb20927d4153ed912815
Mahmoud Ghazal <mahmoudomar480a@gmail.com>
2024-08-12 19:53:07 +0300

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BMI Demo',
+      title: 'Mahmoud Project',
       theme: ThemeData(
         // This is the theme of your application.
         //
